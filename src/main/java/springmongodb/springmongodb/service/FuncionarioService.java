c366a73059f766f6b5e5547587e35f5aa4f0fbb6
Caitano <josiasmartinscaitano@gmail.com>
2022-04-22 17:32:58 -0300

    fixture: adicionado o enum do service

---
+++ src/main/java/springmongodb/springmongodb/service/FuncionarioService.java
@@ -0,0 +1,12 @@
+package springmongodb.springmongodb.service;
+
+import java.util.List;
+
+public interface FuncionarioService {
+
+    public List<Funcionario> obterTodos();
+
+    public Funcionario obterPorCodigo(String codigo);
+
+    public Funcionario criar(Funcionario funcionario);
+}
